9ed197b8b00eed239d29ed79ef05857e82160f10
Nam Khoa <khoa.pham28042002@hcmut.edu.vn>
2022-11-23 23:44:04 +0700

    added vehicle info

---
+++ uwc/src/components/VehicleInformation/index.js
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+
+
+
+const VehicleInformationComponent = ()=>{
+
+    const [bigBoxValue,updateBigBoxValue] = useState("(Currently no description)")
+
+
+
+    return(
+        <div style={{position:"relative", left:"25%", width:"60%"}}>
+            <div style={{position:"relative", left:"15%", fontSize:"xx-large", padding:"15px", fontWeight:"bold"}}>Vehicle Id</div>
+            <div style={{border: "solid", borderRadius:"10px"}}>
+                <div style={{ fontSize:"x-large", paddingBottom:"15px", paddingLeft:"10px"}}>Vehicle Information</div>
+                <div style={{display:"flex", justifyContent:"flex-start", padding:"15px 0px", paddingLeft:"10px", paddingBottom:"25px"}}>
+                    <div style={{display  : "flex", flexDirection : "column"}}>
+                        <label style={{paddingBottom:"5px"}}>Weight</label>
+                        <input type="text" style={{height:"30px", borderRadius:"5px"}}></input>
+                    </div>
+
+                    <div style={{paddingLeft: "20%", display  : "flex", flexDirection : "column"}}>
+                        <label style={{paddingBottom:"5px"}}>Capacity</label>
+                        <input type="text" style={{height:"30px", borderRadius:"5px"}}></input>
+                    </div>
+                </div>
+                <div style={{paddingLeft:"10px"}}>
+                    <div style={{ display : "flex", flexDirection : "column"}}>
+                        <label style={{paddingBottom:"5px"}}>Description</label>
+                        <input value={bigBoxValue} onChange={(e)=>{updateBigBoxValue(e.currentTarget.value)}} type="text" style={{height:"100px", width:"300px", borderRadius:"5px", paddingBottom:"50px"}}></input>
+                    </div>
+                </div>
+                <div style={{padding:"40px 0px 20px 0px", display:"flex", position:"relative", left:"60%"}}>
+                    <button style={{borderRadius:"5px",width:"70px", height:"50px"}}>Cancel</button>
+                    <button style={{borderRadius:"5px",width:"70px", height:"50px", marginLeft:"40px", backgroundColor:"dodgerblue"}}>Submit</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default VehicleInformationComponent
--- uwc/src/pages/VehiclePage/index.js
+++ uwc/src/pages/VehiclePage/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import NavBar from '../../components/NavBar'
+import VehicleInformationComponent from '../../components/VehicleInformation'
 
 function VehiclePage() {
   return (
     <div className='vehicle'>
       <NavBar />
       Vehicle Page
+      <VehicleInformationComponent>
+        
+      </VehicleInformationComponent>
     </div>
   )
 }
