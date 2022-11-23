fd398e00543ac27581fb3c27c2851b9ba3e3d6a8
Thanh Nhan <85187276+NhanVo-gmt@users.noreply.github.com>
2022-11-23 22:47:23 +0700

    add headbar
    adding headbar js and css

---
+++ uwc/src/components/HeadBarComponent/HeadBarInformation.js
@@ -0,0 +1,16 @@
+import "./HeadBar.css";
+
+function HeadBarInformation() {
+    return (
+      <div className='headbar'>
+        <div>
+          <img className="image" src = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Status_iucn_EN_icon_blank.svg/1024px-Status_iucn_EN_icon_blank.svg.png" alt = "Notification" width = "50px"></img>
+          <img className="image"  src = "https://cdn-icons-png.flaticon.com/512/55/55281.png" alt = "Notification" width = "50px"></img>
+          <img className="image"  src = "https://cdn-icons-png.flaticon.com/512/1380/1380370.png" alt = "Notification" width = "50px"></img>
+          <img className="image"  src = "https://as2.ftcdn.net/v2/jpg/03/64/76/01/1000_F_364760110_JP1XMi9xkg9N8a6exTywUYpr2ETXdMem.jpg" alt = "Notification" width = "50px"></img>
+        </div>
+      </div>
+    )
+  }
+  
+  export default HeadBarInformation
--- uwc/src/pages/DashboardPage/index.js
+++ uwc/src/pages/DashboardPage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import HeadBarInformation from '../../components/HeadBarComponent/HeadBarInformation'
 import NavBar from '../../components/NavBar'
 
 function DashboardPage() {
   return (
     <div className='dashboard'>
         <NavBar />
+        <HeadBarInformation></HeadBarInformation>
     </div>
   )
 }
