97a15e30f5ddf80334aa0cd6fadea7f49cf72a57
Quan Pham Anh <ethanpham.lhp@gmail.com>
2022-11-24 23:04:08 +0700

    fix: redirect default route to login

--- uwc/src/pages/VehiclePage/index.js
+++ uwc/src/pages/VehiclePage/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import NavBar from '../../components/NavBar'
-import VehicleInformationComponent from '../../components/VehicleInformation'
+import React from "react";
+import NavBar from "../../components/NavBar";
+import VehicleInformationComponent from "../../components/VehicleInformation";
 
 function VehiclePage() {
   return (
-    <div className='vehicle'>
+    <div className="vehicle">
       <NavBar />
       <VehicleInformationComponent />
     </div>
-  )
+  );
 }
 
-export default VehiclePage
+export default VehiclePage;
