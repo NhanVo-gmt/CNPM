63e0a68077d26f6d68ff845e847f42c6ca8c0258
Quan Pham Anh <ethanpham.lhp@gmail.com>
2022-11-24 22:25:06 +0700

    fix: layout

--- uwc/src/pages/LoginPage/index.js
+++ uwc/src/pages/LoginPage/index.js
@@ -7,33 +7,40 @@ import logo from "../../assets/logo.png";
 function LoginPage() {
   return (
     <div className="login">
-      <img
-        src={cover}
-        alt=""
-        className="cover"
-      />
+      <img src={cover} alt="" className="cover" />
       <div className="container">
-          <p className="header">Sign in</p>
-          <form method="POST" className="textbox">
-            <label for="name" className="label">Username</label>
-            <input type="text" id="name" placeholder="Enter your username" name="username" className="form-input" required />
+        <p className="header">Sign in</p>
+        <form method="POST" className="textbox">
+          <label for="name" className="label">
+            Username
+          </label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter your username"
+            name="username"
+            className="form-input"
+            required
+          />
 
-            <label for="password" className="label">Password</label>
-            <input type="password" id="password" placeholder="Enter your password" name="password" className="form-input" required />
-            
-            <Link to="/dashboard">
+          <label for="password" className="label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            name="password"
+            className="form-input"
+            required
+          />
+
+          <Link to="/dashboard">
             <input type="submit" className="button" value="Sign in" />
-            </Link>
-            
-          
-      </form>
-      
+          </Link>
+        </form>
       </div>
-      <img
-            src={logo}
-            alt=""
-            className="logo-login"
-      />
+      <img src={logo} alt="" className="logo-login" />
     </div>
   );
 }
